bc01a8893f5f4d445851ec819a13689d09d3e4c3
abdusj728 <109174322+abdusj728@users.noreply.github.com>
2022-07-12 22:10:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = exports = {
-    "bot_token": os.environ['DISCORD_TOKEN']
+    "bot_token": os.environ['DISCORD_TOKEN'],
     "prefix": "!", // bot prefix
     "id_length": 10, // length of file ids
     "max_file_size": 1e+7, // max file size in bytes
